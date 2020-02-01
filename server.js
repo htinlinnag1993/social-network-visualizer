f0cc029c1850bcbaef5c17cd6b1ebc8fef51e28f
unknown <htinlinnag1993@gmail.com>
2020-02-01 02:39:11 -0500

    routing in production added the first part

--- server.js
+++ server.js
@@ -16,5 +16,12 @@ app.get('/api/generate/random-profiles-and-connections', function(req, res) {
     // rpg.reset();
 });
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets like our main.js file, main.css file
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html file if it doesn't recognize the route
+}
+
 const PORT = process.env.PORT || 5000;
 var server = app.listen(PORT, function() {});
