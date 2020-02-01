bee5c539d80e17537bd04dd39bef41d322fff8b2
unknown <htinlinnag1993@gmail.com>
2020-02-01 02:41:49 -0500

    routing in production added the second part

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     // Express will serve up the index.html file if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    }); 
 }
 
 const PORT = process.env.PORT || 5000;
